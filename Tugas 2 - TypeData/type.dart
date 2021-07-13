fed509068059af70dfed2be983b19a4937777dcb
yogialfin23 <43507570+yogialfin23@users.noreply.github.com>
2021-07-13 22:02:42 +0700

    Add files via upload

---
+++ Tugas 2 - TypeData/type.dart
@@ -0,0 +1,83 @@
+// SOAL 1
+
+// void main() {
+//   var word = "Dart";
+//   var second = "is";
+//   var third = "awesome";
+//   var fourth = "and";
+//   var fifth = "I";
+//   var sixth = "love";
+//   var seventh = "it!";
+
+//   print("$word $second $third $fourth $fifth $sixth $seventh");
+// }
+
+//SOAL 2
+
+// void main() {
+//   var sentence = "I am going to be Flutter Developer";
+//   var exampleFirstWord = sentence[0];
+//   var exampleSecondWord = sentence[2] + sentence[3];
+//   var exampleThirdWord =
+//       sentence[5] + sentence[6] + sentence[7] + sentence[8] + sentence[9];
+//   var exampleFourthWord = sentence[11] + sentence[12];
+//   var exampleFifthWord = sentence[14] + sentence[15];
+//   var exampleSixthWord = sentence[17] +
+//       sentence[18] +
+//       sentence[19] +
+//       sentence[20] +
+//       sentence[21] +
+//       sentence[22] +
+//       sentence[23];
+//   var exampleSeventhWord = sentence[25] +
+//       sentence[26] +
+//       sentence[27] +
+//       sentence[28] +
+//       sentence[29] +
+//       sentence[30] +
+//       sentence[31] +
+//       sentence[32] +
+//       sentence[33];
+
+//   print("First Word = $exampleFirstWord");
+//   print("Second Word = $exampleSecondWord");
+//   print("Third Word = $exampleThirdWord");
+//   print("Fourth Word = $exampleFourthWord");
+//   print("Fifth Word = $exampleFifthWord");
+//   print("Sixth Word = $exampleSixthWord");
+//   print("Seventh Word = $exampleSeventhWord");
+// }
+
+//SOAL 3
+
+// import 'dart:io';
+
+// void main() {
+//   print("Masukkan Nama Depan : ");
+//   String? namaDepan = stdin.readLineSync();
+//   print("Masukkan Nama Belakang : ");
+//   String? namaBelakang = stdin.readLineSync();
+//   print("Nama Lengkap Anda Adalah : ");
+//   print("$namaDepan $namaBelakang");
+// }
+
+// SOAL 4
+
+// import 'dart:io';
+
+// void main() {
+//   print("Masukkan angka pertama : ");
+//   int? a = int.parse(stdin.readLineSync()!);
+//   print("Masukkan angka kedua : ");
+//   int? b = int.parse(stdin.readLineSync()!);
+
+//   int perkalian = a * b;
+//   double pembagian = a / b;
+//   int penambahan = a + b;
+//   int pengurangan = a - b;
+
+//   print("Perkalian : $perkalian ");
+//   print("Pembagian : $pembagian");
+//   print("Penambahan : $penambahan");
+//   print("Pengurangan : $pengurangan");
+// }
